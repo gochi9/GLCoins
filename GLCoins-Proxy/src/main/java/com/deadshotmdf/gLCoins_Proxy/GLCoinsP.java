cdd13109a06d0d76c7ebb05142a6d7210f18f2b3
gochi9 <gochijoaca@yahoo.com>
2024-09-28 12:32:49 +0300

    Initial commit

---
+++ GLCoins-Proxy/src/main/java/com/deadshotmdf/gLCoins_Proxy/GLCoinsP.java
@@ -0,0 +1,23 @@
+package com.deadshotmdf.gLCoins_Proxy;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "glcoins-proxy",
+        name = "GLCoins-Proxy",
+        version = "1.0-SNAPSHOT"
+        , authors = {"DeadshotMDF"}
+)
+public class GLCoinsP {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
---
+++ GLCoins-Server/src/main/java/com/deadshotmdf/gLCoins_Server/GLCoinsS.java
@@ -0,0 +1,17 @@
+package com.deadshotmdf.gLCoins_Server;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GLCoinsS extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
